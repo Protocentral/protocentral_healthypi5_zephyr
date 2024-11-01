ff092ef1919694b9e582d6b095b9cb82a021815f
Ashwin K Whitchurch <ashwin@protocentral.com>
2024-11-01 10:07:49 +0530

    Temp commit

--- make_basic.sh
+++ make_basic.sh
@@ -1,1 +1,3 @@
-west build -p always -b healthypi5 app -DEXTRA_CONF_FILE=overlay-bt.conf -DEXTRA_CONF_FILE=overlay-logger-sd.conf -DEXTRA_DTC_OVERLAY_FILE=healthypi5_rp2040_sd.overlay 
+west build -p always -b healthypi5 app -DEXTRA_CONF_FILE='overlay-logger-sd.conf;overlay-bt.conf' -DEXTRA_DTC_OVERLAY_FILE=healthypi5_rp2040_sd.overlay 
+
+#DEXTRA_CONF_FILE=overlay-bt.conf
