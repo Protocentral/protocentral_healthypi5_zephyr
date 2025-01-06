cd5f27c13e5a431e42a881e6924cf800eb71315b
MishalFerrao <51846137+MishalJasmine@users.noreply.github.com>
2025-01-06 14:55:26 +0530

    update temp unit in display_module.c

--- app/src/display/display_module.c
+++ app/src/display/display_module.c
@@ -500,7 +500,7 @@ void draw_scr_home_footer(lv_obj_t *parent)
 
     // Temp Sub deg C label
     lv_obj_t *label_temp_sub = lv_label_create(parent);
-    lv_label_set_text(label_temp_sub, "°C");
+    lv_label_set_text(label_temp_sub, "°F");
     lv_obj_align_to(label_temp_sub, label_temp, LV_ALIGN_BOTTOM_MID, 0, 10);
     lv_obj_add_style(label_temp_sub, &style_sub, LV_STATE_DEFAULT);
 
