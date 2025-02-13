3b04c2a10f280178fa2320fdf78d2e0743343ffd
Ashwin K Whitchurch <ashwin@protocentral.com>
2025-02-13 14:24:56 +0530

    Remove unused code

--- app/src/sampling_module.c
+++ app/src/sampling_module.c
@@ -32,92 +32,7 @@ SENSOR_DT_READ_IODEV(afe4400_iodev, DT_ALIAS(afe4400), {SENSOR_CHAN_RED});
 
 extern struct k_sem sem_ecg_bioz_thread_start;
 
-/*RTIO_DEFINE_WITH_MEMPOOL(max30001_read_rtio_ctx,
-                         16,
-                         16, 
-                         32, 
-                         32, 
-                         4   
-);*/
-
-/*RTIO_DEFINE_WITH_MEMPOOL(afe4400_read_rtio_ctx,
-                         16, 
-                         16, 
-                         32, 
-                         32, 
-                         4   
-);*/
-
-/*
-static void sampling_thread(void)
-
-{
-    printk("Sampling Thread starting\n");
-
-    int sample_count = 0;
-
-    int32_t last_read_temp_value = 0;
-
-    for (;;)
-    {
-        k_sleep(K_MSEC(SAMPLING_INTERVAL_MS));
-
-        //struct sensor_value red_sample;
-        //struct sensor_value ir_sample;
-
-
-        sensor_sample_fetch(afe4400_dev);
-        sensor_channel_get(afe4400_dev, SENSOR_CHAN_RED, &red_sample);
-        sensor_channel_get(afe4400_dev, SENSOR_CHAN_IR, &ir_sample);
-
-        struct hpi_sensor_data_t sensor_sample;
-
-
-        sensor_sample.raw_red = red_sample.val1;
-        sensor_sample.raw_ir = ir_sample.val1;
-        sensor_sample.temp = last_read_temp_value;
-        sensor_sample._bioZSkipSample = false;
-        sensor_sample.rtor = rtor_sample.val1;
-        sensor_sample.hr = hr_sample.val1;
-
-
-        if (sample_count >= TEMP_SAMPLING_INTERVAL_COUNT)
-        {
-            sample_count = 0;
-            sensor_sample_fetch(max30205_dev);
-            struct sensor_value temp_sample;
-            sensor_channel_get(max30205_dev, SENSOR_CHAN_AMBIENT_TEMP, &temp_sample);
-            // Convert to degree F
-            if (temp_sample.val1 > 0)
-            {
-                last_read_temp_value = temp_sample.val1; //(temp_sample.val1 * 9 / 5) + 32000;
-            }
-            // printk("Temp: %d\n", last_read_temp_value);
-        }
-        else
-        {
-            sample_count++;
-        }
-
-        // printk("%d ", sensor_sample.ecg_sample);
-
-        // k_msgq_put(&q_sample, &sensor_sample, K_NO_WAIT);
-    }
-}
-*/
-
 static volatile int hpi_sampling_ppg_sample_count = 0;
-
-/*static void sensor_ppg_process_cb(int result, uint8_t *buf, uint32_t buf_len, void *userdata)
-{
-    const struct afe4400_encoded_data *edata = (const struct afe4400_encoded_data *)buf;
-
-    struct hpi_ppg_sensor_data_t ppg_sensor_sample;
-
-    ppg_sensor_sample.ppg_red_sample = edata->raw_sample_red;
-    ppg_sensor_sample.ppg_ir_sample = edata->raw_sample_ir;
-    k_msgq_put(&q_ppg_sample, &ppg_sensor_sample, K_MSEC(1));
-}*/
 
 static void sensor_ppg_decode(uint8_t *buf, uint32_t buf_len)
 {
@@ -134,51 +49,6 @@ static void sensor_ppg_decode(uint8_t *buf, uint32_t buf_len)
         //k_msgq_purge(&q_ppg_sample);
     }
 }
-
-/*static void sensor_ecg_bioz_processing_cb(int result, uint8_t *buf,
-                                          uint32_t buf_len, void *userdata)
-{
-    const struct max30001_encoded_data *edata = (const struct max30001_encoded_data *)buf;
-    struct hpi_ecg_bioz_sensor_data_t ecg_bioz_sensor_sample;
-
-    //printk("ECG NS: %d | B: %d ", edata->num_samples_ecg, edata->num_samples_bioz);
-    uint8_t n_samples_ecg = edata->num_samples_ecg;
-    uint8_t n_samples_bioz = edata->num_samples_bioz;
-
-    if (n_samples_ecg > 8)
-    {
-        n_samples_ecg = 8;
-    }
-
-    if (n_samples_bioz > 4)
-    {
-        n_samples_bioz = 4;
-    }
-
-    if ((n_samples_ecg > 0) || (n_samples_bioz > 0))
-    {
-        ecg_bioz_sensor_sample.ecg_num_samples = n_samples_ecg;
-        ecg_bioz_sensor_sample.bioz_num_samples = n_samples_bioz;
-
-        for (int i = 0; i < n_samples_ecg; i++)
-        {
-            ecg_bioz_sensor_sample.ecg_samples[i] = edata->ecg_samples[i];
-        }
-
-        for (int i = 0; i < n_samples_bioz; i++)
-        {
-            ecg_bioz_sensor_sample.bioz_samples[i] = edata->bioz_samples[i];
-        }
-
-        ecg_bioz_sensor_sample.hr = edata->hr;
-
-        while(k_msgq_put(&q_ecg_bioz_sample, &ecg_bioz_sensor_sample, K_MSEC(2))!=0)
-        {
-            LOG_ERR("Q Err");
-            k_msgq_purge(&q_ecg_bioz_sample);
-        }
-    }
-}*/
 
 static void sensor_ecg_bioz_decode(uint8_t *buf, uint32_t buf_len)
 {
